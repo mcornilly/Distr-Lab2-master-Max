ae47335b3978cb73decc5a368ed6edbf0cde6c7d
mcornilly <79586304+mcornilly@users.noreply.github.com>
2022-04-06 09:30:06 +0200

    Delete BankingApplication-master directory

--- BankingApplication-master/BankingApplication-master/src/main/java/springbootstarter/repositories/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package springbootstarter.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springbootstarter.bankingObjects.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
--- BankingApplication-master/BankingApplication-master/src/main/java/springbootstarter/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package springbootstarter.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springbootstarter.bankingObjects.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
